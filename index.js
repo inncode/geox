62cc72e53b5fd12b69e9a6df3e309d757d364c91
Hiago Silva Souza <hiasilva@gmail.com>
2019-05-22 20:45:21 -0300

    Improvements

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ const chalk       = require('chalk');
 const clear       = require('clear');
 const figlet      = require('figlet');
 const inquirer    = require('./lib/inquirer');
-const utils    = require('./lib/utils');
 const package = require('./package.json');
 
 clear();
 console.log(chalk.yellow(figlet.textSync('GeoX', { horizontalLayout: 'full' })));
 console.log(chalk.blue("Geolocation tools / by hiago.me"));
 console.log(chalk.red("Version " + package.version));
-if(utils.checkGeoliteCityExists()) {
-    console.log(chalk.yellow('[Licence] This product includes GeoLite2 data created by MaxMind, available from https://www.maxmind.com'));
-}
 
 const run = () => {
     inquirer.disposeActions().then((result) => {
