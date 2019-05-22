54d37cc876341e8166c1afc5eb3e35e7e380811b
Hiago Silva Souza <hiasilva@gmail.com>
2019-05-22 17:59:20 -0300

    First version of geox application

---
+++ README.md
@@ -0,0 +1,21 @@
+# GEOX
+This is a tool to facilitate finding positions of IP addresses, address positions and addresses.
+
+## Installing
+To install the library, simply use the package manager npm and enter the command: `npm install -g @inncode/geox`
+
+## Using
+To use GEOX simply enter the `geox` command on your command line and follow the instructions.
+
+## Development
+To contribute to the project, just make a fork of this repository, after that make the fork clone, run the `npm install` command. After that make your changes, run `npm link`, test your modifications and commit and request the pull request.
+
+Author
+----
+> [Hiago Silva Souza](https://www.hiago.me) <<hiasilva@gmail.com>>
+
+License
+----
+
+MIT -
+**Free Software, Hell Yeah!**
---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const chalk       = require('chalk');
+const clear       = require('clear');
+const figlet      = require('figlet');
+const inquirer    = require('./lib/inquirer');
+const utils    = require('./lib/utils');
+
+clear();
+console.log(chalk.yellow(figlet.textSync('GeoX', { horizontalLayout: 'full' })));
+// console.log(chalk.blue("Geolocation tools / by hiago.me"));
+console.log(chalk.red("Version 1.0.0"));
+if(utils.checkGeoliteCityExists()) {
+    console.log(chalk.yellow('[Licence] This product includes GeoLite2 data created by MaxMind, available from https://www.maxmind.com'));
+}
+
+const run = () => {
+    inquirer.disposeActions().then((result) => {
+        if(result.options !== 'exit') {
+            inquirer[result.options]().then((r) => {
+                run();
+            }).catch((error) => {
+                console.log(chalk.red('Error: ', error));
+                run();
+            });
+        }
+    }).catch((error) => {
+        console.log(chalk.red('Error: ', error));
+        run();
+    });
+}
+
+run();
